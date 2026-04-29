a5a3a5db6a61d89c406e4a56e17849d0dd912c10
山田　祥大 <yamadashota@yamadasachidainoMacBook-Pro-2.local>
2026-04-29 22:09:34 +0900

    exmple3.sqlのミスを修正

--- sql/example3.sql
+++ sql/example3.sql
@@ -5,6 +5,6 @@ FROM branches
 INNER JOIN branch_products
 ON branch_products.branch_code = branches.branch_code
 AND branch_products.office_code = branches.office_code
-GROUP BY office_code, branch_code
+GROUP BY branches.office_code, branches.branch_code
 ORDER BY product_count DESC
 LIMIT 1;
